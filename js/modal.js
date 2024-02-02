bdb900117d77b769dc52722827560932f49bd464
andriiKrakovskyi <andrii.krakovskyi@gmail.com>
2024-02-02 22:36:42 +0100

    mob-fix-js

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,18 @@
 		refs.modal.classList.toggle("is-open");
 	}
 })();
+
+(() => {
+	const refs = {
+		openMobModalBtn: document.querySelector("[mob-modal-open]"),
+		closeMobModalBtn: document.querySelector("[mob-modal-close]"),
+		mobModal: document.querySelector("[mob-modal]"),
+	};
+
+	refs.openMobModalBtn.addEventListener("click", toggleModal);
+	refs.closeMobModalBtn.addEventListener("click", toggleModal);
+
+	function toggleModal() {
+		refs.mobModal.classList.toggle("is-open");
+	}
+})();
